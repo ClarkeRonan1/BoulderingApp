0eca5e8d2d1cafedbad1765453bf4c0dcdf39924
ronanclarke <c13357611@mydit.ie>
2018-03-15 13:50:59 +0000

    Update: - Successfully added new route_image to the Firebase Storage. - Haven't tried sending information into the Posts collection yet. - Got the code from the Updating user profile Name and image activity. - Next step is to read the satus and image from the database, inflate the adapter and display on the newsfeed.

--- app/src/main/java/com/example/ronanclarke/boulderingapp/AddPostActivity.java
+++ app/src/main/java/com/example/ronanclarke/boulderingapp/AddPostActivity.java
@@ -65,8 +65,7 @@ public class AddPostActivity extends AppCompatActivity
     private FirebaseFirestore firebaseFirestore;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
 
@@ -91,17 +90,14 @@ public class AddPostActivity extends AppCompatActivity
         addPostBTN = findViewById(R.id.add_post_button);
 
         //Implement Onclick Listener on Image for cropping image
-        climbWallPic.setOnClickListener(new View.OnClickListener()
-        {
+        climbWallPic.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
 
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-                {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     //If user has not already allowed permission to gallery
-                    if(ContextCompat.checkSelfPermission(AddPostActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    if (ContextCompat.checkSelfPermission(AddPostActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
 
                         //Display error message
                         Toast.makeText(AddPostActivity.this, "Permission Denied", Toast.LENGTH_LONG).show();
@@ -110,8 +106,7 @@ public class AddPostActivity extends AppCompatActivity
 
                     }
                     //Open image picker
-                    else
-                    {
+                    else {
                         //Call function in order to crop photo
                         cropImage();
 
@@ -119,8 +114,7 @@ public class AddPostActivity extends AppCompatActivity
 
                 }
                 // Open image picker if sdk is less than Marshmallow
-                else
-                {
+                else {
                     //Call function in order to crop photo
                     cropImage();
 
@@ -132,6 +126,125 @@ public class AddPostActivity extends AppCompatActivity
             }
         });
 
+        //Copied and pasted from UserProfileActivity
+        //Listen on setup button for on click
+        addPostBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //create local variable to hold the user Status
+                final String user_status = userStatus.getText().toString();
+
+                //Check if username is filled out and that image has been selected
+                if (!TextUtils.isEmpty(user_status) && climbWallPicURI != null) {
+
+
+                    //check whether the username has been changed from the default
+                    if (isChanged) {
+                        //get current user id
+                        userID = mAuth.getCurrentUser().getUid();
+
+                        //Make connection to profile images within Users collection
+                        StorageReference storedFiles = storageReference.child("route_images").child(userID + ".jpg");
+                        //Use the image url to put the username alongside the image in profile images collection
+                        storedFiles.putFile(climbWallPicURI).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                //If successfully connected
+                                if (task.isSuccessful()) {
+                                    //add the username to Firestore
+                                    firestoreHandler(task, user_status);
+
+                                }
+                                //If connection not made successfully
+                                else {
+
+                                    String error = task.getException().getMessage();
+                                    //Display error message
+                                    Toast.makeText(AddPostActivity.this, "(IMAGE Error) : " + error, Toast.LENGTH_LONG).show();
+
+
+                                }
+                            }
+                        });
+
+                    }
+                    //User has not changed their username
+                    else {
+                        //Call function storeFirestore, send null to specify that
+                        firestoreHandler(null, user_status);
+
+                    }
+
+                }
+
+            }
+
+        });
+
+    }
+    //Copied and pasted from UserProfileActivity
+
+    //Handler for Firestore
+    private void firestoreHandler(@NonNull Task<UploadTask.TaskSnapshot> task, String userStatus)
+    {
+        //get firestore URL
+        Uri download_uri;
+
+        //Task needs to be carried out so get URL, this case need to add username
+        if(task != null)
+        {
+
+            download_uri = task.getResult().getDownloadUrl();
+
+        }
+        //No task to be carried out, so just download the information
+        else
+        {
+
+            download_uri = climbWallPicURI;
+
+        }
+
+        //Encrypt the information
+        Map<String, String> userMap = new HashMap<>();
+
+        userMap.put("name", userStatus);
+        userMap.put("image", download_uri.toString());
+
+        //Set new username and user image using users id
+        firebaseFirestore.collection("Posts").document(userID).set(userMap).addOnCompleteListener(new OnCompleteListener<Void>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<Void> task)
+            {
+                //If username successfully added to FirebaseFirestore
+                if(task.isSuccessful())
+                {
+                    //Let user know their settings are updated
+                    Toast.makeText(AddPostActivity.this, "The user Settings are updated.", Toast.LENGTH_LONG).show();
+                    //Send user to the main Activity
+                    Intent mainIntent = new Intent(AddPostActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+
+                }
+                //If username not successfully added to FirebaseFireStore
+                else
+                {
+
+                    String error = task.getException().getMessage();
+                    //Display Error message
+                    Toast.makeText(AddPostActivity.this, "(FIRESTORE Error) : " + error, Toast.LENGTH_LONG).show();
+
+                }
+
+
+            }
+        });
+
+
+    }
+        /*
         //Onclick listener for add new post button
         addPostBTN.setOnClickListener(new View.OnClickListener()
         {
@@ -147,12 +260,28 @@ public class AddPostActivity extends AppCompatActivity
                 {
                     //Give this status a unique ID
                     final String assignID = UUID.randomUUID().toString();
+                    //Reference to the storage file in Firebase, check within Storage for
+                    //The name of the file, here is the profile_images and route_images
+                    StorageReference storedFiles = storageReference.child("route_images");
+                    storedFiles.putFile(climbWallPicURI).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task)
+                        {
+                            final String UriCheck = task.getResult().getDownloadUrl().toString();
+                            //if climbWallPucURI is valid url
+                            if(task.isSuccessful())
+                            {
+
+                            }
+                        }
+                    })
 
                 }
             }
         });
+        */
 
-    }
+
 
     //function to crop image
     private void cropImage()
